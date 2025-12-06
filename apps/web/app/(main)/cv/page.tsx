bb451de0a216aecfc1099d0228a573aca3861c6c
SBOZH <sem.bozhyk@sbozh.me>
2025-12-06 03:45:02 +0100

    fix(cv): hide page title in print view

--- apps/web/app/(main)/cv/page.tsx
+++ apps/web/app/(main)/cv/page.tsx
@@ -32,7 +32,7 @@ export default function CVPage() {
 
   return (
     <div className="mx-auto px-4 md:px-12 lg:px-24 py-6 md:py-12 print:p-0">
-      <h2 className="text-center">Founder's CV</h2>
+      <h2 className="text-center print:hidden">Founder's CV</h2>
       <div className="flex justify-center print:hidden z-10 flex gap-2 p-4 py-8 px-8">
         <CVThemeToggle />
         <CVPrintButton />
